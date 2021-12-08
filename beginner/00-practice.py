6f70fbdb16de90e2ea84d5c04579ad86d5e9a665
Abhishek Chaudhary <chaudha4@hotmail.com>
2021-12-08 18:04:55 -0500

    day 6 part 1

--- beginner/00-practice.py
+++ beginner/00-practice.py
@@ -1,15 +1,6 @@
 
-x1, y1, x2, y2 = 9,7,9,9
+a = [0] * 8
 
-xstep, ystep = 1, 1
-
-if (x1 > x2):
-    xstep = -1
-
-if (y1 > y2):
-    ystep = -1
-
-for xx, yy in zip(range(x1, x2, xstep), range(y1, y2, ystep)):
-    print(xx, yy)
+print(a)
 
 
