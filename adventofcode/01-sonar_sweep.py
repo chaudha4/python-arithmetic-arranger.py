cc399f7646a14aa909d4b1d92e7e0683bb248e7a
Abhishek Chaudhary <you@example.com>
2022-12-07 17:53:05 -0500

    Updated Day1 puzzle with a more efficient logic.

--- adventofcode/01-sonar_sweep.py
+++ adventofcode/01-sonar_sweep.py
@@ -2,11 +2,11 @@
 https://adventofcode.com/2021/day/1
 """
 
-def part1():
+def puzzle1():
     with open("adventofcode/01-sonar_sweep.txt", "r") as f:
         #data = f.read()
         data = f.read().split()
-        print(len(data))
+        #print(len(data))
         
         count = 0
 
@@ -24,22 +24,34 @@ def part1():
 
             prev = curr
 
-        print(count)
+        print(f'Found measurements {count} than the previous measurement. Expected 1482')
 
 
-def part2():
+def puzzle2():
 
+    testdata = [199,200,208,210,200,207,240,269,260,263]
     data = []
     with open("adventofcode/01-sonar_sweep.txt", "r") as f:
         data = [ int(d) for d in f.read().split() ]
 
-    print(data)
+    #print(data)
     count = 0
     for ii in range(len(data)-3):
         if (data[ii+1] + data[ii+2] + data[ii+3]) > (data[ii+1] + data[ii+2] + data[ii]):
             count = count+1
 
-    print(count)
+    print(f'Three-measurement sliding window count is {count}. Expected 1518.')
 
-part1()
-part2()
+    # A more efficient way would be to only look at the first and fourth. If 4th is more than Ist, the sliding 
+    # window sum must be increasing since 2nd and 3rd are common in both windows.
+    # Found this here - https://youtu.be/zcunDS2RoRk
+
+    count = 0
+    for ii in range(len(data)-3):
+        #print(ii,data[ii],data[ii+3])
+        if (data[ii] < data[ii+3]):
+            count += 1
+    print(f'Three-measurement sliding window count is {count}. Expected 1518.')
+
+puzzle1()
+puzzle2()
